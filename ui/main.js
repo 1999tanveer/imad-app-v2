32b3c65af401ca67b7ffb3eef605f0b60876d5ff
1999tanveer <1999tanveer@gmail.com>
2017-02-11 13:28:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,19 @@
 console.log('Loaded!');
+
 //Changing the content in html.
+var element = document.getElementById('intro');
+
+element.innerHTML='Hai I am Salih';
+
+//Animating the image to move.
+var img= document.getElementId('salih');
+var leftmar=0;
+
+function moveright() {
+ leftmar=leftmar+10;
+ img.style.marginleft = leftmar+'px';
+}
+
+img.onclick=function() {
+var interval = setInterval(moveright,20)
+}
