b3d2180fa71db456c4a189cb8b6c80e6b58a1f37
1999tanveer <1999tanveer@gmail.com>
2017-02-14 20:26:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,37 @@
 console.log('Loaded!');
-
-//Changing the content in html.
-//var element = document.getElementById('intro');
-
-//element.innerHTML='Hai I am Salih';
-
-//Animating the image to move.
+                    
+                    //Changing the content in html.
+                    //var element = document.getElementById('intro');
+                    
+                    //element.innerHTML='Hai I am Salih';
+                    
+                    //Animating the image to move.
 var img = document.getElementById('salih');
 var marginleft=0;
 
 function moveright() {
- if(marginleft<950) {
+if(marginleft<950) {
      marginleft=marginleft+10;
      img.style.marginLeft = marginleft + 'px';
  }
- else{
+else{
      marginleft=-900;
      img.style.marginLeft = marginleft + 'px';
  }
     
 }
 
-img.onclick=function() {
-   var interval = setInterval(moveright,20);
+mg.onclick=function() {
+  var interval = setInterval(moveright,20);
 };
+
+
+
+
+var button = document.getElementById("counter");
+button.onclick=function(){
+    counter=counter+1;    
+    var span = document.getElementById("count");
+    element.innerHTML = counter.toString();
+    
+}
