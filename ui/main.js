bba0dafe7137aa4a97b35dfb030889dda89ff927
1999tanveer <1999tanveer@gmail.com>
2017-02-14 21:03:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,13 +30,23 @@ img.onclick=function() {
 var counter=0;
 var button = document.getElementById("counter");
 button.onclick=function(){
-    //Request couter end pt
+    
+    //create a XML req
+       var request= new XMLHttpRequest();
     
     //capture the response and store it in a variable
+       request.onreadystatechange = function() {
+           
+         if(request.readyState===XMLHttpRequest.DONE) {
+             if(request.status===200){
+                 var counter=request.responseText;
+                 var span = document.getElementById("count");
+                 span.innerHTML = counter.toString();
+             }
+         }  
+       };
     
-    //Rendering the variable
-    counter = counter + 1;    
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-    
-}
+    //Making the request
+       request.open('GET','http://user-1999tanveer.imad.hasura-app.io/counter',true);
+       request.send(null);
+};
